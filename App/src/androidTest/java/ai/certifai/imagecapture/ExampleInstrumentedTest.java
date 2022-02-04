0ec2cba7781479c04598951d8704eec38c61d061
yinchuangsum <yinchuang96@gmail.com>
2022-02-04 20:41:32 +0800

    update

--- App/src/androidTest/java/ai/certifai/imagecapture/ExampleInstrumentedTest.java
+++ App/src/androidTest/java/ai/certifai/imagecapture/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ai.certifai.FruitClassifier", appContext.getPackageName());
+        assertEquals("ai.certifai.imagecapture", appContext.getPackageName());
     }
 }
